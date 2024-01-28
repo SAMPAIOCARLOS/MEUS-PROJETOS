a4a0c47eef1031035571a8887be0be3ac53b7b0d
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-01-28 18:02:41 -0300

    novas partes

--- new-project/scripts/functions.js
+++ new-project/scripts/functions.js
@@ -10,6 +10,41 @@ function validarName(name, seletor) {
     }
 }
 
+function validarPhone (campo, seletor) {
+    if(campo.value.length < 11) {
+        campo.style.border = '1px solid rgb(186, 0, 0)'
+        document.querySelector(seletor).style.display = 'block'
+        campo.focus()
+        return
+    } else {
+        campo.style.border = 'none'
+        document.querySelector(seletor).style.display = 'none'
+    }
+}
+
+function validarEmail (campo, seletor) {
+    if(campo.value === '' || !isEmailValue(campo.value)) {
+        campo.style.border = '1px solid rgb(186, 0, 0)'
+        document.querySelector(seletor).style.display = 'block'
+        campo.focus()
+        return
+    } else {
+        campo.style.border = 'none'
+        document.querySelector(seletor).style.display = 'none'
+    }
+}
+
+function validarEmail_conf (campoEmail, campoConfEmail, seletor) {
+    if(campoEmail.value === campoConfEmail.value) {
+        campoConfEmail.style.border = 'none'
+        document.querySelector(seletor).style.display = 'none'
+    } else {
+        campoConfEmail.style.border = '1px solid rgb(186, 0, 0)'
+        document.querySelector(seletor).style.display = 'block'
+        campoConfEmail.focus()
+        return
+    }
+}
 
 function vazio(campo, seletor) {
     if(campo.value === '') {
--- new-project/scripts/group.js
+++ new-project/scripts/group.js
@@ -1,30 +1,20 @@
-const form_grou = document.getElementById('form-group')
-const solicitante = document.getElementById('Solicitante')
-const Data_visita_group = document.getElementById('Data-visita-group')
-const telefone_group = document.getElementById('telefone-group')
-const email_group = document.getElementById('email-group')
-const conf_email_group = document.getElementById('conf-email-group')
-const rua = document.getElementById('rua')
-const N = document.getElementById('N')
-const bairro = document.getElementById('bairro')
-const cep = document.getElementById('cep')
-const cidade = document.getElementById('city')
-const select_case_yes = document.getElementById('select-case-yes')
-const select_case_yes_escolaridade = document.getElementById('select-case-yes-escolaridade')
-const Faixa_etaria_yes = document.getElementById('Faixa-etaria-yes')
-const Quantidade_yes = document.getElementById('Quantidade-yes')
-const name_instituicao_case_no = document.getElementById('name-instituicao-case-no')
-const Faixa_etaria_no = document.getElementById('Faixa-etaria-no')
-const Quantidade_no = document.getElementById('Quantidade-no')
-const campo_def_case_yes = document.getElementById('campo-def-case-yes').value
-const campo_def_case_no = document.getElementById('campo-def-case-no').value
+ const form_grou = document.getElementById('form-group')
+ const solicitante = document.getElementById('Solicitante')
+ const telefone_group = document.getElementById('telefone-group')
+ const email_group = document.getElementById('email-group')
+ const conf_email_group = document.getElementById('conf-email-group')
+ const select_deficiencia = document.getElementById('select-deficiencia')
+ const select_responsavel_presente = document.getElementById('select-responsavel-presente')
+ const Nome_responsavel_case_No = document.getElementById('Nome-responsavel-case-No')
+ const telefone_responsavel_case_No = document.getElementById('telefone-responsavel-case-No')
+ const email_responsavel_case_No = document.getElementById('email-responsavel-case-No')
+ const conf_email_group_responsavel = document.getElementById('conf-email-group-responsavel')
+ const mySelect_estados = document.getElementById('mySelect-estados')
+
+// const case_yes = document.getElementById('case_yes')
+// const case_no = document.getElementById('case_no')
 
 
-const case_yes = document.getElementById('case_yes')
-const case_no = document.getElementById('case_no')
-
-
-const select_estado = document.getElementById('select-estado')
 const estados_brasil = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
 
@@ -33,69 +23,132 @@ for (let i = 0; i < estados_brasil.length; i++) {
     const newOption = document.createElement('option')
     newOption.innerText = estado
 
-    select_estado.append(newOption)
+    mySelect_estados.append(newOption)
 }
 
-function isEmailValue(email) { 
+
+
+// async function mudarCidade() {
+//     const res = await fetch(``)
+// }
+
+
+
+
+// const select_responsavel_presente = document.getElementById('select-responsavel-presente')
+
+select_responsavel_presente.addEventListener('change', ()=> {
+    if(select_responsavel_presente.value === 'Nao') {
+        document.getElementById('box-responsavel-case-No').style.display = 'flex'     
+    } else{
+        document.getElementById('box-responsavel-case-No').style.display = 'none'
+    }
+})
+
+
+select_deficiencia.addEventListener('change', ()=> {
+    if(select_deficiencia.value === 'sim') {
+        document.getElementById('section-infor-def').style.display = 'flex'
+    } else {
+        document.getElementById('section-infor-def').style.display = 'none'
+    }
+})
+
+
+
+
+
+function isEmailValue(campo) { 
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/;
 
-    return emailRegex.test(email);
+    return emailRegex.test(campo);
 }
 
-function validarCEP(cep) {
+// function validarCEP(cep) {
     
-    var regexCEP = /^[0-9]{5}-?[0-9]{0,3}$/;
+//     var regexCEP = /^[0-9]{5}-?[0-9]{0,3}$/;
 
   
-    if (regexCEP.test(cep)) {
-      return true; // CEP válido
-    } else {
-      return false; // CEP inválido
-    }
-  }
-
-function isTelefoneValue() {
-    telefone_group.addEventListener('keyup', ()=> {
-             
-        var apenasNumeros = telefone_group.value.replace(/\D/g, '');
-
-        if (apenasNumeros.length <= 2) {
-            telefone_group.value = apenasNumeros;
-        } else if (apenasNumeros.length <= 6) {
-            telefone_group.value = '(' + apenasNumeros.slice(0, 2) + ') ' + apenasNumeros.slice(2);
-        } else {
-            telefone_group.value = '(' + apenasNumeros.slice(0, 2) + ') ' + apenasNumeros.slice(2, 6) + '-' + apenasNumeros.slice(6);
-        }
-  
-    });
-}
+//     if (regexCEP.test(cep)) {
+//       return true; // CEP válido
+//     } else {
+//       return false; // CEP inválido
+//     }
+//   }
 
 
+const telefones = document.querySelectorAll('.telefones')
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    isTelefoneValue()
+telefones.forEach(phone => {
+
+    document.addEventListener('DOMContentLoaded', ()=> {
+
+        phone.addEventListener('keyup', ()=> {
+                 
+            let apenasNumeros = phone.value.replace(/\D/g, '');
+    
+            if (apenasNumeros.length <= 2) {
+                phone.value = apenasNumeros;
+            } else if (apenasNumeros.length <= 6) {
+                phone.value = '(' + apenasNumeros.slice(0, 2) + ') ' + apenasNumeros.slice(2);
+            } else {
+                phone.value = '(' + apenasNumeros.slice(0, 2) + ') ' + apenasNumeros.slice(2, 6) + '-' + apenasNumeros.slice(6);
+            }
+      
+        });
+    })    
+    
 });
 
 
-const box_case_yes = document.getElementById('box-case-yes')
-const box_case_no = document.getElementById('box-case-no')
 
-document.getElementById('case_yes').addEventListener('click', (event)=> {
-    box_case_no.style.display = 'none'
-    box_case_yes.style.display = 'flex'
 
-    document.getElementById('case_no').checked = false
-})
+// function isTelefoneValue() {
+//     telefone_group.addEventListener('keyup', ()=> {
+             
+//         var apenasNumeros = telefone_group.value.replace(/\D/g, '');
 
-document.getElementById('case_no').addEventListener('change', ()=> {
-    box_case_yes.style.display = 'none'
-    box_case_no.style.display = 'flex'
+//         if (apenasNumeros.length <= 2) {
+//             telefone_group.value = apenasNumeros;
+//         } else if (apenasNumeros.length <= 6) {
+//             telefone_group.value = '(' + apenasNumeros.slice(0, 2) + ') ' + apenasNumeros.slice(2);
+//         } else {
+//             telefone_group.value = '(' + apenasNumeros.slice(0, 2) + ') ' + apenasNumeros.slice(2, 6) + '-' + apenasNumeros.slice(6);
+//         }
+  
+//     });
+// }
 
-    document.getElementById('case_yes').checked = false
-})
+
+
+
+// document.addEventListener("DOMContentLoaded", ()=> {
+//     isTelefoneValue()
+// });
+
+
+// const box_case_yes = document.getElementById('box-case-yes')
+// const box_case_no = document.getElementById('box-case-no')
+
+// document.getElementById('case_yes').addEventListener('click', (event)=> {
+//     box_case_no.style.display = 'none'
+//     box_case_yes.style.display = 'flex'
+
+//     document.getElementById('case_no').checked = false
+// })
+
+// document.getElementById('case_no').addEventListener('change', ()=> {
+//     box_case_yes.style.display = 'none'
+//     box_case_no.style.display = 'flex'
+
+//     document.getElementById('case_yes').checked = false
+// })
 
 form_grou.addEventListener('submit', (event)=> {
     event.preventDefault()
+
+
+    
 
     if(solicitante.value.length < 3) {
         validarName(solicitante, '.span-name-group')
@@ -104,170 +157,113 @@ form_grou.addEventListener('submit', (event)=> {
         validarName(solicitante, '.span-name-group')
     }
 
-    let valorCampo = Data_visita_group.value;
-    let dataHora = new Date(valorCampo);
+    // let valorCampo = Data_visita_group.value;
+    // let dataHora = new Date(valorCampo);
 
-    if (dataHora < new Date()) {
-        Data_visita_group.style.border = '1px solid rgb(186, 0, 0)'
-        document.getElementById('icon-data-hora-group').style.display = 'block'
-        document.getElementById('campo-data-visita-group').innerText = 'Você tem que selecionar uma data maior que a atual!'
-        Data_visita_group.focus()
-        return 
-    }
+    // if (dataHora < new Date()) {
+    //    Data_visita_group.style.border = '1px solid rgb(186, 0, 0)'
+    //    document.getElementById('icon-data-hora-group').style.display = 'block'
+    //    document.getElementById('campo-data-visita-group').innerText = 'Você tem que selecionar uma data maior que a atual!'
+    //    Data_visita_group.focus()
+    //    return 
+    // }
 
-    if (!isNaN(dataHora.getTime())) {
-      if (dataHora.getDay() === 1) {
-        Data_visita_group.style.border = '1px solid rgb(186, 0, 0)'
-        document.getElementById('icon-data-hora-group').style.display = 'block'
-        document.getElementById('campo-data-visita-group').innerText = 'Não funcionamos as segundas-feiras!'
-        Data_visita_group.focus()
-        return
-      } else{
-        Data_visita_group.style.border = 'none'
-        document.getElementById('icon-data-hora-group').style.display = 'none'
-        document.getElementById('campo-data-visita-group').innerText = ''
-      }
-    } else {
-        Data_visita_group.style.border = '1px solid rgb(186, 0, 0)'
-        document.getElementById('icon-data-hora-group').style.display = 'block'
-        document.getElementById('campo-data-visita-group').innerText = 'Preencha este campo!'
-        Data_visita_group.focus()
-        return
-    }
+    // if (!isNaN(dataHora.getTime())) {
+    //   if (dataHora.getDay() === 1) {
+    //     Data_visita_group.style.border = '1px solid rgb(186, 0, 0)'
+    //     document.getElementById('icon-data-hora-group').style.display = 'block'
+    //     document.getElementById('campo-data-visita-group').innerText = 'Não funcionamos as segundas-feiras!'
+    //     Data_visita_group.focus()
+    //     return
+    //   } else{
+    //     Data_visita_group.style.border = 'none'
+    //     document.getElementById('icon-data-hora-group').style.display = 'none'
+    //     document.getElementById('campo-data-visita-group').innerText = ''
+    //   }
+    // } else {
+    //     Data_visita_group.style.border = '1px solid rgb(186, 0, 0)'
+    //     document.getElementById('icon-data-hora-group').style.display = 'block'
+    //     document.getElementById('campo-data-visita-group').innerText = 'Preencha este campo!'
+    //     Data_visita_group.focus()
+    //     return
+    // }
 
     if(telefone_group.value.length < 11) {
-        telefone_group.style.border = '1px solid rgb(186, 0, 0)'
-        document.querySelector('.span-telefone-group').style.display = 'block'
-        telefone_group.focus()
+        validarPhone(telefone_group, '.span-telefone-group')
         return
     } else {
-        telefone_group.style.border = 'none'
-        document.querySelector('.span-telefone-group').style.display = 'none'
+        validarPhone(telefone_group, '.span-telefone-group')
     }
 
 
     if(email_group.value === '' || !isEmailValue(email_group.value)) {
-        email_group.style.border = '1px solid rgb(186, 0, 0)'
-        document.querySelector('.span-email-group').style.display = 'block'
-        email_group.focus()
+        validarEmail(email_group, '.span-email-group')
         return
     } else {
-        email_group.style.border = 'none'
-        document.querySelector('.span-email-group').style.display = 'none'
+        validarEmail(email_group, '.span-email-group')
     }
 
     if(email_group.value === conf_email_group.value) {
-        conf_email_group.style.border = 'none'
-        document.querySelector('.span-conf-email-group').style.display = 'none'
+        validarEmail_conf(email_group, conf_email_group, '.span-conf-email-group')
     } else {
-        conf_email_group.style.border = '1px solid rgb(186, 0, 0)'
-        document.querySelector('.span-conf-email-group').style.display = 'block'
-        conf_email_group.focus()
+        validarEmail_conf(email_group, conf_email_group, '.span-conf-email-group')
         return
     }
 
     
-
-    if(rua.value === '') {
-        vazio(rua, '.span-invalido-rua')
+    if(select_responsavel_presente.value === '') {
+        vazio(select_responsavel_presente, '.span-responsavel-presente')
         return
     } else {
-        vazio(rua, '.span-invalido-rua')
+        vazio(select_responsavel_presente, '.span-responsavel-presente')
     }
 
-    if(N.value === '') {
-        vazio(N, '.span-invalido-N')
-        return
-    } else {
-        vazio(N, '.span-invalido-N')
-    }
 
-    if(bairro.value === '') {
-        vazio(bairro, '.span-invalido-bairro')
-        return
-    } else {
-        vazio(bairro, '.span-invalido-bairro')
-    }
 
-    if(cep.value === '' || !validarCEP(cep.value)) {
-        cep.style.border = '1px solid rgb(186, 0, 0)'
-        document.querySelector('.span-invalido-cep').style.display = 'block'
-        cep.focus()
-        return
-    } else {
-        cep.style.border = 'none'
-        document.querySelector('.span-invalido-cep').style.display = 'none'
-    }
+    if(select_responsavel_presente.value === 'Nao') {
+        
+        if(Nome_responsavel_case_No.value.length < 3) {
+            validarName(Nome_responsavel_case_No, '.span-name-case-responsavel-no')
+            return
+        } else {
+            validarName(Nome_responsavel_case_No, '.span-name-case-responsavel-no')
+        }
 
-    if(cidade.value === '') {
-        vazio(cidade, '.span-invalido-city')
-        return
-    } else {
-        vazio(cidade, '.span-invalido-city')
-    }
-
-    if(select_estado.value === '') {
-        vazio(select_estado, '.span-invalido-estado')
-        return
-    } else{
-        vazio(select_estado, '.span-invalido-estado')
-    }
-//
-
-    if(case_yes.checked === true) {
-
-        if(select_case_yes.value === '') {
-            vazio(select_case_yes, '.span-invalido-instituicao')
+        if(telefone_responsavel_case_No.value.length < 11) {
+            validarPhone(telefone_responsavel_case_No, '.span-telefone-case-responsavel-no')
             return
         } else{
-            vazio(select_case_yes, '.span-invalido-instituicao')
-        }
-    
-        if(select_case_yes_escolaridade.value === '') {
-            vazio(select_case_yes_escolaridade, '.span-invalido-escolaridade')
-            return
-        } else{
-            vazio(select_case_yes_escolaridade, '.span-invalido-escolaridade')
-        }
-    
-        if(Faixa_etaria_yes.value === '') {
-            vazio(Faixa_etaria_yes, '.span-invalido-faixaEtaria')
-            return
-        } else {
-            vazio(Faixa_etaria_yes, '.span-invalido-faixaEtaria')
-        }
-    
-        if(Quantidade_yes.value === '') {
-            vazio(Quantidade_yes, '.span-invalido-quantidade')
-            return
-        } else{
-            vazio(Quantidade_yes, '.span-invalido-quantidade')
-        }
-    } else {
-        if(name_instituicao_case_no.value === '') {
-            vazio(name_instituicao_case_no, '.span-name-instituicao-case-no')
-            return
-        } else {
-            vazio(name_instituicao_case_no, '.span-name-instituicao-case-no')
-        }
-    
-        if(Faixa_etaria_no.value === '') {
-            vazio(Faixa_etaria_no, '.span-invalido-faixaEtaria-case-no')
-            return
-        } else {
-            vazio(Faixa_etaria_no, '.span-invalido-faixaEtaria-case-no')
+            validarPhone(telefone_responsavel_case_No, '.span-telefone-case-responsavel-no')
         }
 
-        if(Quantidade_no.value === '') {
-            vazio(Quantidade_no, '.span-invalido-quantidade-case-no')
+
+        if(email_responsavel_case_No.value === '' || !isEmailValue(email_responsavel_case_No.value)) {
+            validarEmail(email_responsavel_case_No, '.span-email-case-responsavel-no')
             return
         } else {
-            vazio(Quantidade_no, '.span-invalido-quantidade-case-no')
+            validarEmail(email_responsavel_case_No, '.span-email-case-responsavel-no')
+        }
+    
+        if(email_responsavel_case_No.value === conf_email_group_responsavel.value) {
+            validarEmail_conf(email_responsavel_case_No, conf_email_group_responsavel, '.span-conf-email-case-responsavel-no')
+        } else {
+            validarEmail_conf(email_responsavel_case_No, conf_email_group_responsavel, '.span-conf-email-case-responsavel-no')
+            return
         }
 
+        
+
+          
     }
-    
 
+    if(mySelect_estados.value === '') {
+        vazio(mySelect_estados, '.span-select-estado')
+        return
+    } else {
+        vazio(mySelect_estados, '.span-select-estado')
+    }
 
 })
+
+
 
