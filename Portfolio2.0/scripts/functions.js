741268bd74f8b928aa4370d19c0724f8e7d4b4e3
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-04-14 15:41:44 -0300

    fazendo a parte sobre

--- Portfolio2.0/scripts/functions.js
+++ Portfolio2.0/scripts/functions.js
@@ -1,13 +1,17 @@
+const text = document.getElementById("span_auto_imcrement")
+const array_text_about_header = ['Desenvolvedor Front-end','Front-end Developer','Desenvolvedor Front-end','Front-end Developer']
+//Biblioteca typed. js
+
+function typed(id, array) {
+    const typed = new Typed(id, {
+        strings: array,
+        typeSpeed: 80, //tempo para os caracteres serem escritos
+        backSpeed: 40, //tempo para os caracteres serem apagados
+        cursorChar: "|", //O simbolo que aparece no final
+        loop: true //Para ficar se repetindo infinitamente
+    })
+}
 
 
-const typed = new Typed("#span_auto_imcrement", {
-    strings: [
-        'Desenvolvedor Front-end',
-        'Sou muito macho',
-        'E gosto de mulher'
-    ],
-    typeSpeed: 80,
-    backSpeed: 40,
-    cursorChar: "|",
-    loop: true
-})
+
+typed(text, array_text_about_header)
