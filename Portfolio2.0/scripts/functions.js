f6157833e5525159dbb0563b998271b2fb5fb48f
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-04-14 13:52:56 -0300

    parte 1 feita

---
+++ Portfolio2.0/scripts/functions.js
@@ -0,0 +1,13 @@
+
+
+const typed = new Typed("#span_auto_imcrement", {
+    strings: [
+        'Desenvolvedor Front-end',
+        'Sou muito macho',
+        'E gosto de mulher'
+    ],
+    typeSpeed: 80,
+    backSpeed: 40,
+    cursorChar: "|",
+    loop: true
+})
