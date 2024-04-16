2c2392e6570c4c8d1f58e96415723547628e3e36
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-04-16 12:36:40 -0300

    alterando

---
+++ v2/scripts/functions.js
@@ -0,0 +1,17 @@
+const text = document.getElementById("span_auto_imcrement")
+const array_text_about_header = ['Desenvolvedor Front-end','Front-end Developer','Desenvolvedor Front-end','Front-end Developer']
+//Biblioteca typed. js
+
+function typed(id, array) {
+    const typed = new Typed(id, {
+        strings: array,
+        typeSpeed: 80, //tempo para os caracteres serem escritos
+        backSpeed: 40, //tempo para os caracteres serem apagados
+        cursorChar: "|", //O simbolo que aparece no final
+        loop: true //Para ficar se repetindo infinitamente
+    })
+}
+
+
+
+typed(text, array_text_about_header)
