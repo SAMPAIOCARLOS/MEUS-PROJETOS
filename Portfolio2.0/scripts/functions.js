f3f33508b02f21f2290150d00ece67177865bbf6
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-04-19 11:17:58 -0300

    alteração

--- Portfolio2.0/scripts/functions.js
+++ Portfolio2.0/scripts/functions.js
@@ -1,5 +1,12 @@
-const text = document.getElementById("span_auto_imcrement")
-const array_text_about_header = ['Desenvolvedor Front-end','Front-end Developer','Desenvolvedor Front-end','Front-end Developer']
+const text = document.getElementById("span_auto_imcrement");
+const array_text_about_header = ['Desenvolvedor Front-end','Front-end Developer','Desenvolvedor Front-end','Front-end Developer'];
+
+const hamburguer = document.getElementById(`hamburguer`);
+const list_full = document.getElementById("list-full");
+const button_nav = document.querySelector(".button-nav");
+
+const link_navgation = document.querySelectorAll(".link-navgation")
+
 //Biblioteca typed. js
 
 function typed(id, array) {
@@ -11,16 +18,9 @@ function typed(id, array) {
         loop: true //Para ficar se repetindo infinitamente
     })
 }
+typed(text, array_text_about_header);
 
 
-
-typed(text, array_text_about_header)
-
-
-const hamburguer = document.getElementById(`hamburguer`)
-const list_full = document.getElementById("list-full")
-
-const button_nav = document.querySelector(".button-nav")
 
 
 hamburguer.addEventListener("click", ()=> {
@@ -28,5 +28,43 @@ hamburguer.addEventListener("click", ()=> {
     list_full.classList.toggle("active")
 
 })
+
+
+
+console.log(link_navgation)
+
+link_navgation.forEach(element => {
+    element.addEventListener("click", (event)=> {
+        console.log(element)
+
+
+
+        // if(element.innerHTML === "CERTIFICADOS") {
+        //     alert("certificados")
+        // }
+
+        switch (element.innerText) {
+            case "CERTIFICADOS":
+                alert("certificados")
+                
+                break;
+        
+            case "CURSOS":
+                alert("cursos")
+
+                break
+
+            case "TECNOLOGIAS":
+                alert("tecnologias")
+
+                break
+            default:
+
+            alert("se lascar")
+                break;
+        }
+
+    })
+});
 
 
