3307863bfb22f64020d6b9512e3628d571df9ab0
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-06-13 18:36:08 -0300

    parte final

--- project_sejuv/script.js
+++ project_sejuv/script.js
@@ -1,14 +1,18 @@
+const list_full = document.querySelector(".list-full")
+
+const line_span = document.querySelector("line-span")
+
+const hamburguer = document.querySelector(".hamburguer")
+
+
 function trocarImagem() {
         const larguraTela = window.innerWidth;
         let imagem_one = document.getElementById('imagem_center_one');
-        let imagem_two = document.getElementById('imagem_center_two');
 
         if (larguraTela < 1475) {
             imagem_one.src = 'imgs/Ativo 6teste.png';
-            imagem_two.src = 'imgs/Ativo 6teste.png';
         } else {
             imagem_one.src = 'imgs/Ativo 7teste.png';
-            imagem_two.src = 'imgs/Ativo 7teste.png';
         }
     }
 
@@ -17,3 +21,19 @@ function trocarImagem() {
 
     // Trocar a imagem quando a janela for redimensionada
     window.onresize = trocarImagem;
+
+
+
+
+    function Active(Menu_hamburguer, list_full, line_span) {
+        Menu_hamburguer.addEventListener("click", ()=> {
+            Menu_hamburguer.classList.toggle("active")
+            list_full.classList.toggle("active")
+            line_span.classList.toggle("active")
+    
+        })
+    }
+
+    
+
+    Active(hamburguer, list_full, line_span)
