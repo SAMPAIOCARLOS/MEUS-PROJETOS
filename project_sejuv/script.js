d175b04b8bf8156781fe9c46957c749351452e5d
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-06-14 10:59:56 -0300

    alteração

--- project_sejuv/script.js
+++ project_sejuv/script.js
@@ -1,28 +1,40 @@
 const list_full = document.querySelector(".list-full")
-
-const line_span = document.querySelector("line-span")
-
+const line_span = document.querySelector(".line-span")
+const hamb = document.querySelectorAll('.hamburguer span');
 const hamburguer = document.querySelector(".hamburguer")
+const width = 1175
 
 
 function trocarImagem() {
         const larguraTela = window.innerWidth;
-        let imagem_one = document.getElementById('imagem_center_one');
+        const imagem_one = document.getElementById('imagem_center_one');
 
-        if (larguraTela < 1475) {
+        if (larguraTela < width) {
             imagem_one.src = 'imgs/Ativo 6teste.png';
         } else {
             imagem_one.src = 'imgs/Ativo 7teste.png';
         }
+
+        
     }
 
-    // Trocar a imagem quando a página carregar
-    window.onload = trocarImagem;
+    // function trocarImagemTwo() {
+    //     const larguraTela = window.innerWidth;
 
-    // Trocar a imagem quando a janela for redimensionada
-    window.onresize = trocarImagem;
+    //     let imagem_two = document.getElementById('imagem_center_two');
 
+    //     if (larguraTela < width) {
+    //         imagem_two.src = 'imgs/Ativo 6teste.png';
+    //     } else {
+    //         imagem_two.src = 'imgs/Ativo 7teste.png';
+    //     }
 
+        
+    // }
+    // window.onload = trocarImagem;
+    // window.onresize = trocarImagem;
+
+    
 
 
     function Active(Menu_hamburguer, list_full, line_span) {
@@ -30,7 +42,17 @@ function trocarImagem() {
             Menu_hamburguer.classList.toggle("active")
             list_full.classList.toggle("active")
             line_span.classList.toggle("active")
-    
+
+            if (line_span.classList.toggle(".active")) {
+                hamb.forEach(elemnt => {
+                    elemnt.style.backgroundColor = '#000'
+                });
+            } else {
+                hamb.forEach(elemnt => {
+                    elemnt.style.backgroundColor = '#fff'
+                });
+            }
+            
         })
     }
 
