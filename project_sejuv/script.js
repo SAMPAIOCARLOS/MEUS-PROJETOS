cc1793a623b8be2a2e41223734c6a29fb0e4c4c9
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-06-16 13:09:09 -0300

    finalizando responsividade

--- project_sejuv/script.js
+++ project_sejuv/script.js
@@ -1,61 +1,51 @@
-const list_full = document.querySelector(".list-full")
-const line_span = document.querySelector(".line-span")
+const list_full = document.querySelector(".list-full");
+const line_span = document.querySelector(".line-span");
 const hamb = document.querySelectorAll('.hamburguer span');
-const hamburguer = document.querySelector(".hamburguer")
-const width = 1175
+const hamburguer = document.querySelector(".hamburguer");
+const imgs_center = document.querySelectorAll(".testeimg");
+const width = 1175;
 
 
-function trocarImagem() {
-        const larguraTela = window.innerWidth;
-        const imagem_one = document.getElementById('imagem_center_one');
 
-        if (larguraTela < width) {
-            imagem_one.src = 'imgs/Ativo 6teste.png';
+    
+
+window.addEventListener('resize', ()=> {
+    const larguraTela = window.innerWidth;
+
+    if (larguraTela < width) {
+        
+        imgs_center.forEach(image => {
+            image.src = 'imgs/Ativo 6teste.png';
+        });
+    } else {
+
+        imgs_center.forEach(image => {
+            image.src = 'imgs/Ativo 7teste.png';
+        });
+    }
+});
+
+    
+
+
+function Active(Menu_hamburguer, list_full, line_span, hamb) {
+    Menu_hamburguer.addEventListener("click", ()=> {
+        Menu_hamburguer.classList.toggle("active");
+        list_full.classList.toggle("active");
+        line_span.classList.toggle("active");
+
+        if (line_span.classList.toggle(".active")) {
+            hamb.forEach(elemnt => {
+                elemnt.style.backgroundColor = '#000';
+            });
         } else {
-            imagem_one.src = 'imgs/Ativo 7teste.png';
+            hamb.forEach(elemnt => {
+                elemnt.style.backgroundColor = '#fff';
+            });
         }
-
         
-    }
-
-    // function trocarImagemTwo() {
-    //     const larguraTela = window.innerWidth;
-
-    //     let imagem_two = document.getElementById('imagem_center_two');
-
-    //     if (larguraTela < width) {
-    //         imagem_two.src = 'imgs/Ativo 6teste.png';
-    //     } else {
-    //         imagem_two.src = 'imgs/Ativo 7teste.png';
-    //     }
-
-        
-    // }
-    // window.onload = trocarImagem;
-    // window.onresize = trocarImagem;
-
-    
+    })
+}
 
 
-    function Active(Menu_hamburguer, list_full, line_span) {
-        Menu_hamburguer.addEventListener("click", ()=> {
-            Menu_hamburguer.classList.toggle("active")
-            list_full.classList.toggle("active")
-            line_span.classList.toggle("active")
-
-            if (line_span.classList.toggle(".active")) {
-                hamb.forEach(elemnt => {
-                    elemnt.style.backgroundColor = '#000'
-                });
-            } else {
-                hamb.forEach(elemnt => {
-                    elemnt.style.backgroundColor = '#fff'
-                });
-            }
-            
-        })
-    }
-
-    
-
-    Active(hamburguer, list_full, line_span)
+Active(hamburguer, list_full, line_span, hamb);
